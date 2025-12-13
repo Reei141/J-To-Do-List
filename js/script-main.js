8ad900e89274ea8cef888525062842e967653ea4
Reei141 <janeaaly@gmail.com>
2025-12-13 08:40:43 +0700

    update add task

--- js/script-main.js
+++ js/script-main.js
@@ -18,12 +18,12 @@ if (addBtn) {
 function openPop(type) {
     const pop = document.querySelector('.pop');
     const wrapper = document.querySelector('.wrapper');
-    const title = document.querySelector('.pop-content h3');
+    const tittle = document.querySelector('.pop-content h3');
 
     // Ganti judul popup sesuai tipe
-    if (type === "daily") title.textContent = "Add Daily Task";
-    if (type === "monthly") title.textContent = "Add Monthly Task";
-    if (type === "on-date") title.textContent = "Add On-Date Task";
+    if (type === "daily") tittle.textContent = "Add Daily Task";
+    if (type === "monthly") tittle.textContent = "Add Monthly Task";
+    if (type === "on-date") tittle.textContent = "Add On-Date Task";
 
     pop.classList.add('active');
     wrapper.classList.add('inactive'); // blur aktif
@@ -57,6 +57,40 @@ if (addOnDate) {
 const cancelBtn = document.querySelector('.cancel-btn');
 if (cancelBtn) {
     cancelBtn.addEventListener('click', () => {
+        document.querySelector('.pop').classList.remove('active');
+        document.querySelector('.wrapper').classList.remove('inactive'); // hilangin blur
+    });
+}
+
+// SAVE TASK
+const saveBtn = document.querySelector('.save-btn');
+if( saveBtn) {
+    saveBtn.addEventListener('click', () => {
+        const input = document.querySelector('.task-input');
+        const taskText = input.value.trim();
+        const tittle = document.querySelector('.pop-content h3').textContent;
+
+        if (taskText == "") {
+            alert("tASK TIDAK BOLEH KOSONG!");
+            return;
+        }
+
+        // Buat elemen task baru
+        const newTask = document.createElement('div');
+        newTask.classList.add('task-item');
+        newTask.textContent = taskText;
+
+        //Tentukan container berdasarkan tipe tugas
+        if (tittle.includes("Daily")) {
+            document.querySelector('.daily-tasks').appendChild(newTask);
+        } else if (tittle.includes("Monthly")) {
+            document.querySelector('.monthly-tasks').appendChild(newTask);
+        } else if (tittle.includes("On-Date")) {
+            document.querySelector('.on-date-tasks').appendChild(newTask);
+        }
+
+        //Reset input dan tutup popup
+        input.value = "";
         document.querySelector('.pop').classList.remove('active');
         document.querySelector('.wrapper').classList.remove('inactive'); // hilangin blur
     });
